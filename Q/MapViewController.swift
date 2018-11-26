b250cebd0eebe87ce6a62edde096ab0eb1824ac0
jaebooker <jaesonbooker@gmail.com>
2018-11-26 02:08:06 -0800

    Commit the 8th

--- Q/MapViewController.swift
+++ Q/MapViewController.swift
@@ -9,81 +9,139 @@ import UIKit
 import MapKit
 import CoreLocation
 
-final class MapAnnotation: NSObject, MKAnnotation {
-    var coordinate: CLLocationCoordinate2D
-    var title: String?
-    var subtitle: String?
-    
-    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?) {
-        self.coordinate = coordinate
-        self.title = title
-        self.subtitle = subtitle
-        super.init()
-    }
-}
+//final class MapAnnotation: NSObject, MKAnnotation {
+//    var coordinate: CLLocationCoordinate2D
+//    var title: String?
+//    var subtitle: String?
+//
+//    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?) {
+//        self.coordinate = coordinate
+//        self.title = title
+//        self.subtitle = subtitle
+//        super.init()
+//    }
+//}
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     var data = [Restaurant]()
+    var r = Restaurant()
     @IBOutlet var mapView: MKMapView!
     let locationManager = CLLocationManager()
-    let regionInMeters: Double = 100.0
+    var coordinate = CLLocationCoordinate2D()
+    let regionInMeters: Double = 10000.0
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier)
-        let venueCoordinate = CLLocationCoordinate2D(latitude: 37.780664, longitude: -122.416183)
-        let venueAnnotation = MapAnnotation(coordinate: venueCoordinate, title: "Tenderloin", subtitle: "Eaten Them Tender Loins TM")
-        mapView.addAnnotation(venueAnnotation)
-        checkLocationServices()
-    }
-    func setupLocationManager() {
+        mapView.delegate = self
         locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    }
-    func centerViewOnUserLocation() {
-        if let location = locationManager.location?.coordinate {
-            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-            mapView.setRegion(region, animated: true)
-        }
-    }
-    func checkLocationServices() {
-        if CLLocationManager.locationServicesEnabled() {
-            setupLocationManager()
-            checkLocationAuthorization()
-        } else {
-            //
-        }
-    }
-    func checkLocationAuthorization() {
-        switch CLLocationManager.authorizationStatus() {
-        case .authorizedWhenInUse:
-            mapView.showsUserLocation = true
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
-            break
-        case .denied:
-            break
-        case .notDetermined:
-            //ask permision
-            locationManager.requestWhenInUseAuthorization()
-        case .restricted:
-            break
-        case .authorizedAlways:
-            break
-        default:
-            break
-        }
-    }
-}
-extension MapViewController: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        guard let newCoordinates = locationManager.location?.coordinate else { return }
+        let region = MKCoordinateRegion(center: newCoordinates, latitudinalMeters: 500, longitudinalMeters: 500)
         mapView.setRegion(region, animated: true)
+        coordinate = newCoordinates
+        let annotation1 = MKPointAnnotation()
+        annotation1.title = "Bulba Fett Tea"
+        annotation1.subtitle = "Everyone's favourite tea inspired by everyone's favourite Star Trek character"
+        annotation1.coordinate = coordinate
+        mapView.addAnnotation(annotation1)
+        
+        let annotation2 = MKPointAnnotation()
+        annotation2.title = "Kale"
+        annotation2.subtitle = "Kale French Fries!"
+        annotation2.coordinate = CLLocationCoordinate2D(latitude: 37.780664, longitude: -122.416183)
+        mapView.addAnnotation(annotation2)
+        
+//        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier)
+//        let venueCoordinate = CLLocationCoordinate2D(latitude: 37.780664, longitude: -122.416183)
+//        let venueAnnotation = MapAnnotation(coordinate: venueCoordinate, title: "Tenderloin", subtitle: "Eaten Them Tender Loins TM")
+//        mapView.addAnnotation(venueAnnotation)
+//        checkLocationServices()
     }
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        checkLocationAuthorization()
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+        pin.canShowCallout = true
+        pin.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        return pin
     }
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let rView = view.annotation
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let detailVC = storyboard.instantiateViewController(withIdentifier: "restaurantDetail") as! restaurantDetailsTableViewController
+        detailVC.rtitle = ((rView?.title!)!)
+        detailVC.rsubtitle = ((rView?.subtitle)!)!
+        detailVC.rlatitude = (rView?.coordinate.latitude)!
+        detailVC.rlongitude = (rView?.coordinate.longitude)!
+        self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+//    func setupLocationManager() {
+//        locationManager.delegate = self
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//    }
+//    func centerViewOnUserLocation() {
+//        if let location = locationManager.location?.coordinate {
+//            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+//            mapView.setRegion(region, animated: true)
+//        }
+//    }
+//    func checkLocationServices() {
+//        if CLLocationManager.locationServicesEnabled() {
+//            setupLocationManager()
+//            checkLocationAuthorization()
+//        } else {
+//            //
+//        }
+//    }
+//    func checkLocationAuthorization() {
+//        switch CLLocationManager.authorizationStatus() {
+//        case .authorizedWhenInUse:
+//            mapView.showsUserLocation = true
+//            centerViewOnUserLocation()
+//            locationManager.startUpdatingLocation()
+//            break
+//        case .denied:
+//            break
+//        case .notDetermined:
+//            //ask permision
+//            locationManager.requestWhenInUseAuthorization()
+//        case .restricted:
+//            break
+//        case .authorizedAlways:
+//            break
+//        default:
+//            break
+//        }
+//    }
+//    func mapView(_ mapView: MKMapView,annotationView view: MKAnnotationView,calloutAccessoryControlTapped control: UIControl){
+//
+//        r = view.annotation as! Restaurant //get the annotation, which is a parameter
+//
+//        performSegue(withIdentifier: "restaurantDetails", sender: self) //perform manual segue
+//
+//    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//            // Get the new view controller using segue.destinationViewController.
+//            // Pass the selected object to the new view controller.
+//            if segue.identifier=="restaurantDetails" {
+//                let dest = segue.destination as! restaurantDetailsTableViewController
+//                dest.r = r
+//
+//        }
+//
+//    }
+//}
+//extension MapViewController: CLLocationManagerDelegate {
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else { return }
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+//        mapView.setRegion(region, animated: true)
+//    }
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        checkLocationAuthorization()
+//    }
 }
 
 //    var data = [Restaurant]()
