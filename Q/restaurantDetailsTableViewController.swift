fb703ebf5944451c041f9f25414780c7585686de
jaebooker <jaesonbooker@gmail.com>
2018-11-27 07:02:46 -0800

    bottom of the 9th commit

--- Q/restaurantDetailsTableViewController.swift
+++ Q/restaurantDetailsTableViewController.swift
@@ -16,7 +16,8 @@ class restaurantDetailsTableViewController: UITableViewController {
     var rlongitude: Double?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(rtitle)
+        print(rsubtitle)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -28,23 +29,22 @@ class restaurantDetailsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "menuIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = rtitle
+        cell.detailTextLabel?.text = rsubtitle
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
